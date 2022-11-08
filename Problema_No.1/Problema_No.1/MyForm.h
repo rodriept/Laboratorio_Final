b1926d5d7baefcc3a3634fd03b12c0a063cd4c53
Rodrigo <rodriept429@gmail.com>
2022-11-07 23:10:47 -0600

    Encontrar un número dado por el usuario y encontrarlo en el arreglo de forma secuencial

--- Problema_No.1/Problema_No.1/MyForm.h
+++ Problema_No.1/Problema_No.1/MyForm.h
@@ -8,6 +8,9 @@ namespace ProblemaNo1 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
+	using namespace System::Text;
+	using namespace System::Diagnostics;
 
 	/// <summary>
 	/// Resumen de MyForm
@@ -38,6 +41,16 @@ namespace ProblemaNo1 {
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::TextBox^ TBArregloTam;
 	private: System::Windows::Forms::Button^ BIngresarArreglo;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ TBBuscarNumArreglo;
+
+	private: System::Windows::Forms::Button^ Bbuscar;
+	private: System::Windows::Forms::ComboBox^ CBElegirBusqueda;
+
+
+
+
+
 	protected:
 
 	private:
@@ -57,6 +70,10 @@ namespace ProblemaNo1 {
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->TBArregloTam = (gcnew System::Windows::Forms::TextBox());
 			this->BIngresarArreglo = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->TBBuscarNumArreglo = (gcnew System::Windows::Forms::TextBox());
+			this->Bbuscar = (gcnew System::Windows::Forms::Button());
+			this->CBElegirBusqueda = (gcnew System::Windows::Forms::ComboBox());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -74,15 +91,17 @@ namespace ProblemaNo1 {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(12, 57);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(12, 59);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(146, 13);
+			this->label2->Size = System::Drawing::Size(171, 15);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Ingrese el tamaño del arreglo:";
 			// 
 			// TBArregloTam
 			// 
-			this->TBArregloTam->Location = System::Drawing::Point(164, 54);
+			this->TBArregloTam->Location = System::Drawing::Point(189, 58);
 			this->TBArregloTam->Name = L"TBArregloTam";
 			this->TBArregloTam->Size = System::Drawing::Size(47, 20);
 			this->TBArregloTam->TabIndex = 2;
@@ -90,7 +109,7 @@ namespace ProblemaNo1 {
 			// BIngresarArreglo
 			// 
 			this->BIngresarArreglo->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
-			this->BIngresarArreglo->Location = System::Drawing::Point(217, 51);
+			this->BIngresarArreglo->Location = System::Drawing::Point(242, 56);
 			this->BIngresarArreglo->Name = L"BIngresarArreglo";
 			this->BIngresarArreglo->Size = System::Drawing::Size(75, 23);
 			this->BIngresarArreglo->TabIndex = 3;
@@ -98,11 +117,52 @@ namespace ProblemaNo1 {
 			this->BIngresarArreglo->UseVisualStyleBackColor = true;
 			this->BIngresarArreglo->Click += gcnew System::EventHandler(this, &MyForm::BIngresarArreglo_Click);
 			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 116);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(144, 13);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Buscar un número generado:";
+			// 
+			// TBBuscarNumArreglo
+			// 
+			this->TBBuscarNumArreglo->Location = System::Drawing::Point(15, 177);
+			this->TBBuscarNumArreglo->Name = L"TBBuscarNumArreglo";
+			this->TBBuscarNumArreglo->Size = System::Drawing::Size(47, 20);
+			this->TBBuscarNumArreglo->TabIndex = 5;
+			// 
+			// Bbuscar
+			// 
+			this->Bbuscar->Enabled = false;
+			this->Bbuscar->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->Bbuscar->Location = System::Drawing::Point(68, 174);
+			this->Bbuscar->Name = L"Bbuscar";
+			this->Bbuscar->Size = System::Drawing::Size(88, 23);
+			this->Bbuscar->TabIndex = 6;
+			this->Bbuscar->Text = L"Buscar número";
+			this->Bbuscar->UseVisualStyleBackColor = true;
+			this->Bbuscar->Click += gcnew System::EventHandler(this, &MyForm::Bbuscar_Click);
+			// 
+			// CBElegirBusqueda
+			// 
+			this->CBElegirBusqueda->FormattingEnabled = true;
+			this->CBElegirBusqueda->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Secuencial", L"Binaria" });
+			this->CBElegirBusqueda->Location = System::Drawing::Point(15, 136);
+			this->CBElegirBusqueda->Name = L"CBElegirBusqueda";
+			this->CBElegirBusqueda->Size = System::Drawing::Size(121, 21);
+			this->CBElegirBusqueda->TabIndex = 7;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(398, 333);
+			this->Controls->Add(this->CBElegirBusqueda);
+			this->Controls->Add(this->Bbuscar);
+			this->Controls->Add(this->TBBuscarNumArreglo);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->BIngresarArreglo);
 			this->Controls->Add(this->TBArregloTam);
 			this->Controls->Add(this->label2);
@@ -117,6 +177,7 @@ namespace ProblemaNo1 {
 		array<int>^ Arreglo;
 		int Key;
 		Hashtable^ Hashing = gcnew Hashtable;
+		Stopwatch^ TimerControler = gcnew Stopwatch();
 	private: System::Void BIngresarArreglo_Click(System::Object^ sender, System::EventArgs^ e)
 	{
 		try
@@ -132,8 +193,9 @@ namespace ProblemaNo1 {
 				Random^ NumerosRandom = gcnew Random();
 				for (int i = 0; i < Arreglo->Length; i++)
 				{
-					Arreglo[i] = NumerosRandom->Next(0, 100);
+					Arreglo[i] = NumerosRandom->Next(0, 10);
 				}
+				
 				for (int i = 0; i < Arreglo->Length; i++)
 				{
 					int j = 0;
@@ -147,8 +209,9 @@ namespace ProblemaNo1 {
 					Hashing->Add(Key, Arreglo[i]);
 			
 				}
+	
 				MessageBox::Show("El arreglo ya fue llenado con números aleatorios", "Arreglo lleno", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
-				
+				Bbuscar->Enabled = true;
 			}
 		}
 		catch (...)
@@ -159,5 +222,45 @@ namespace ProblemaNo1 {
 	}
 		   
 		  
-	};
+	private: System::Void Bbuscar_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		int opcion = CBElegirBusqueda->SelectedIndex;
+		int EncontrarNum, cont;
+		if (opcion == 1 || opcion == 0)
+		{
+			switch (opcion)
+			{
+			case 0:
+				EncontrarNum = Convert::ToInt32(TBBuscarNumArreglo->Text);
+				cont = 0;
+				for (int i = 0; i < Arreglo->Length; i++)
+				{
+				   if (Arreglo[i] == EncontrarNum)
+				   {
+					 MessageBox::Show("El número se encuentra en la posición: " + i, "Número encontrado", MessageBoxButtons::OK);
+				   }
+				   else
+				   {
+					   cont++;
+				   }
+				   
+				}	
+				if (cont == Arreglo->Length)
+				{
+					MessageBox::Show("No se encuentra el número en el arreglo", "Error:Número no encontrado", MessageBoxButtons::OK, MessageBoxIcon::Error);
+				}
+				break;
+			case 1:
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
+			MessageBox::Show("Ingrese una opcion para buscar el número", "Error: opcion no ingresada", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		}
+		
+	}
+};
 }
